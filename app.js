7ca924428d5eb7025375aabd5e1847b89240d874
Enzo Barcelos <enzobarcelos25@gmail.com>
2026-01-19 20:51:52 -0300

    Alterando valor do numero

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 //let paragrafo = document.querySelector('p');
 //paragrafo.innerHTML = 'Escolha um numero entre 1 e 20';
 let listaDeNumeroSorteados = [];
-let numeroLimite = 30
+let numeroLimite = 20
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
