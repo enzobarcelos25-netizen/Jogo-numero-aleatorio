ec73cbf5cd77f6649e073989b7ac09b809f61a58
Enzo Barcelos <enzobarcelos25@gmail.com>
2026-01-21 21:29:06 -0300

    teste

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 //let paragrafo = document.querySelector('p');
 //paragrafo.innerHTML = 'Escolha um numero entre 1 e 20';
 let listaDeNumeroSorteados = [];
-let numeroLimite = 100000000000000 //deixando jogo impossivel;
+let numeroLimite = 100
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
